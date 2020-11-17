5e41cf6d06fa381b38134ad9d33cbecf31d6b916
Anthony Kinsey <anthonyjk@gmail.com>
2020-11-16 15:34:05 -1000

    wip: working on requests

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import LocalStorageCache from './localStorageCache'
-import Vue from 'vue'
+import { provide } from 'vue'
 
-Vue.prototype.$api = path => fetch(`http://localhost:8000${path}`).then(res => res.json())
-Vue.prototype.$swrvCache = new LocalStorageCache()
+// Vue.prototype.$api = path => fetch(`http://localhost:8000${path}`).then(res => res.json())
+// Vue.prototype.$swrvCache = new LocalStorageCache()
 
 createApp(App).use(router).mount('#app')
+
+provide('$api', path => fetch(`http://localhost:8000${path}`).then(res => res.json()));
+provide('$swrvCache', new LocalStorageCache());
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,42 @@
 <template>
   <h1>Home Page</h1>
+  <p>{{boards}}</p>
+  <p><strong>{{error}}</strong></p>
 </template>
+
+<script>
+import useSWRV from 'swrv'
+import { inject } from 'vue'
+
+export default {
+  name: 'Boards',
+  props: {},
+  setup() {
+    const $api = inject('$api');
+    const $swrvCache = inject('$swrvCache');
+
+    const { data: boards, error: error } = useSWRV(`/api/boards`, path => $api(`${path}`), {
+      cache: $swrvCache
+    })
+
+    return {
+      boards,
+      error
+    }
+  }
+}
+// export default {
+//   data() {
+//     return {
+//       boards: null,
+//       error: null
+//     };
+//   },
+//   created() {
+//     // Simple GET request using axios
+//     const { data: boards, error } = useSWRV(`/boards`, path => root.$api(`${path}`), {
+//       cache: root.$swrvCache
+//     })
+//   }
+// }
+</script>
