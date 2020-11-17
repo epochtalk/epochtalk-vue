9c9d3bad2ed65bc79f2dbbdf87c671c48b9cc699
Anthony Kinsey <anthonyjk@gmail.com>
2020-11-16 16:07:35 -1000

    fix: issue with data containing non latin-1 chars

--- src/localStorageCache.js
+++ src/localStorageCache.js
@@ -1,9 +1,26 @@
+function toBinary(string) {
+  const codeUnits = new Uint16Array(string.length);
+  for (let i = 0; i < codeUnits.length; i++) {
+    codeUnits[i] = string.charCodeAt(i);
+  }
+  return String.fromCharCode(...new Uint8Array(codeUnits.buffer));
+}
+
+function fromBinary(binary) {
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < bytes.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return String.fromCharCode(...new Uint16Array(bytes.buffer));
+}
+
 class LocalStorageCache {
   constructor () {
     this.STORAGE_KEY = 'swrv'
   }
-  encode (storage) { return btoa(JSON.stringify(storage)) }
-  decode (storage) { return JSON.parse(atob(storage)) }
+
+  encode (storage) { return btoa(toBinary(JSON.stringify(storage))) }
+  decode (storage) { return JSON.parse(fromBinary(atob(storage))) }
 
   get (k) {
     const item = localStorage.getItem(this.STORAGE_KEY)
