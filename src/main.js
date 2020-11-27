be38eaaeba3109d615a4c068b3224172a4102bc6
Bronson Oka <boka@slickage.com>
2020-11-27 12:18:21 -1000

    feat: use vuex store in main

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import localStorageCache from './localStorageCache'
+import Vuex from './store/vuex'
 
 const app = createApp(App).use(router)
+app.use(Vuex);
 
 app.provide('$api', path => fetch(`http://localhost:8080${path}`).then(res => res.json()))
 app.provide('$swrvCache', localStorageCache)
