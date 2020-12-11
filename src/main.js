1e6aa4baf1b96f4b8e3c641b6ba310fc06da60ad
Anthony Kinsey <anthonyjk@gmail.com>
2020-12-10 14:15:45 -1000

    feat: set default login

--- src/main.js
+++ src/main.js
@@ -8,14 +8,18 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 
 const app = createApp(App).use(router)
 
-
 const appCache = localStorageCache(0, 'app')
+
+const auth = appCache.get('auth')
+const user = auth ? auth.data : undefined
 
 const $axios = axios.create({
   baseURL: 'http://localhost:8080',
   timeout: 3000,
   crossDomain: true
 })
+
+if (user) { $axios.defaults.headers.common['Authorization'] = `BEARER ${user.token}` }
 
 app.provide('$axios', $axios)
 
