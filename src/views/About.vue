f530e152e82492b092ec89009e9d6a975af27894
Anthony Kinsey <anthonyjk@gmail.com>
2021-01-11 12:47:48 -1000

    feat: implement skeleton about view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1>About Page</h1>
+  <div class="about-section">
+    <h1 class="view-title">About </h1>
+    <div class="description"></div>
+
+    <span class="copyright">&copy; </span>
+  </div>
+
+  <div class="sidebar">
+    <ul class="about-links">
+      <span class="section-header">
+        Legal Info
+      </span>
+      <li>
+        <a ng-href="/legal#tos" target="_blank" rel="noopener noreferrer">
+          Terms of Service
+        </a>
+      </li>
+      <li>
+        <a ng-href="/legal#privacy" target="_blank" rel="noopener noreferrer">
+          Privacy Policy
+        </a>
+      </li>
+      <li>
+        <a ng-href="/legal#disclaimer" target="_blank" rel="noopener noreferrer">
+          Disclaimer
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+</style>
