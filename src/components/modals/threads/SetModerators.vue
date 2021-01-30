35f5387ab663867042a21e153c6e02cb9acf9715
Anthony Kinsey <anthonyjk@gmail.com>
2021-01-29 14:27:04 -1000

    feat: wip implement api calls for set moderator

--- src/components/modals/threads/SetModerators.vue
+++ src/components/modals/threads/SetModerators.vue
@@ -24,7 +24,7 @@
 
 <script>
 import Modal from '@/components/layout/Modal.vue'
-import { cloneDeep } from 'lodash'
+import { intersection, remove } from 'lodash'
 import { reactive, toRefs, inject } from 'vue'
 // import { AuthStore } from '@/composables/stores/auth'
 import Multiselect from '@vueform/multiselect'
@@ -37,14 +37,74 @@ export default {
   setup(props, { emit }) {
     /* Template Methods */
     const setModerators = () => {
-      console.log('Set Moderators!', originalModList, v.modTagsInput.value, v.modsToRemove)
+      const modsToAdd = v.modTagsInput.value
+      const modsToRemove = v.modsToRemove
+      const mods = props.board.moderators
+      console.log('Set Moderators!', mods, modsToAdd, modsToRemove)
 
       // Compare against original mod list, if net add/remove of mods is same just alert success
 
       // Call API to remove mods on v.modsToRemove
 
       // Call API to add mods on v.modsTagsInput.value
-      close()
+
+      // figure out which are duplicates
+      let inter = intersection(modsToAdd, modsToRemove)
+      inter.forEach(interName => {
+        remove(modsToAdd, name => name === interName)
+        remove(modsToRemove, name => name === interName)
+      })
+
+      // build save params
+      let addParams = { usernames: modsToAdd, board_id: props.board.id };
+      let removeParams = { usernames: modsToRemove, board_id: props.board.id };
+
+      // remove moderators if needed
+      return new Promise(resolve => {
+        if (!modsToRemove.length) return resolve()
+
+        let promise = $axios.post('/api/admin/moderators/remove', removeParams)
+        .then(res => res.status === 200 ? res.data : res)
+        .then(users => {
+          users.forEach(user => {
+            console.log(remove(mods, mod => mod.username === user.username))
+          })
+        })
+
+        return resolve(promise);
+      })
+      // add moderators if needed
+      .then(() => {
+        if (!modsToAdd.length) return
+
+        return $axios.post('/api/admin/moderators', addParams)
+        .then(res => res.status === 200 ? res.data : res)
+        .then(users => {
+          users.forEach(user => mods.push({ username: user.username, id: user.id }))
+          return users
+        })
+      })
+      .then(function() { $alertStore.success('Moderators successfully updated'); })
+      .catch(function() { $alertStore.error('There was an error updating moderators'); })
+      .finally(() => {
+        close()
+      })
+      // Moderators.add(addParams).$promise
+      //   .then(function(users) {
+      //     users.forEach(function(user) {
+      //       ctrl.board.moderators.push({ username: user.username, id: user.id });
+      //     });
+      //     return users;
+      //   })
+      //   .then(function(users) { return ctrl.checkPermissions(users); })
+      //   .then(function(bpUsers) { ctrl.usersWithBadPermissions = bpUsers; });
+      // })
+      // .then(function() { ctrl.mods = angular.copy(ctrl.board.moderators); })
+      // .then(function() { Alert.success('Moderators successfully updated'); })
+      // .catch(function() { Alert.error('There was an error updating moderators'); })
+      // .finally(function() {
+      //   if (!ctrl.usersWithBadPermissions.length) { ctrl.closeModerators(); }
+      // });
     }
 
     const removeModerator = username => {
@@ -60,7 +120,8 @@ export default {
     /* Internal Data */
     // const auth = inject(AuthStore)
     const $axios = inject('$axios')
-    const originalModList = cloneDeep(props.board.moderators)
+    const $alertStore = inject('$alertStore')
+    // const originalModList = cloneDeep(props.board.moderators)
 
     /* Template Data */
     // const initMods = []
