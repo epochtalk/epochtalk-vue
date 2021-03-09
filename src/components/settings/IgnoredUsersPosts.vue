ac10156259091b2eaf62794329d94ac46f443ce3
Anthony Kinsey <anthonyjk@gmail.com>
2021-03-09 12:49:54 -1000

    feat: skeleton for IgnoredUsersPosts component

---
+++ src/components/settings/IgnoredUsersPosts.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="ignored-users-posts-settings" class="settings-section">
+    <h3 class="thin-underline">Ignored Users Posts</h3>
+
+
+  </div>
+</template>
+
+<script>
+import { reactive, inject, onBeforeMount, toRefs } from 'vue'
+import { threadsApi } from '@/api'
+
+export default {
+  name: 'ignored-users-posts',
+  setup() {
+    onBeforeMount(() => v.ignored = [])
+    /* View Methods */
+    const ignoreUser = () => console.log('Ignored Users Posts')
+
+    // const $alertStore = inject('$alertStore')
+
+    const v = reactive({ ignored: [] })
+
+    return { ignoreUser, ...toRefs(v) }
+  }
+}
+</script>
