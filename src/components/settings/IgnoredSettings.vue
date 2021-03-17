060af380922d039cc351181118c87c017e1762c6
Anthony Kinsey <anthonyjk@gmail.com>
2021-03-16 15:44:11 -1000

    feat: port over mention/mention setting switches and convert template code to vue, still need to hook up

--- src/components/settings/IgnoredSettings.vue
+++ src/components/settings/IgnoredSettings.vue
@@ -7,9 +7,47 @@
       <a class="button" :class="{ 'selected': selectedTab === 'mentions' }" @click.prevent="selectedTab = 'mentions'">Mentions</a>
     </div>
     <div class="tabs-content">
-      <ignored-settings-partial :api="usersApi" v-show="selectedTab === 'posts'" />
-      <ignored-settings-partial :api="messagesApi" v-show="selectedTab === 'messages'" />
-      <ignored-settings-partial :api="mentionsApi" v-show="selectedTab === 'mentions'" />
+      <div v-show="selectedTab === 'posts'">
+        <ignored-settings-partial :api="usersApi" />
+      </div>
+      <div v-show="selectedTab === 'messages'">
+        <div class="toggle-switch-container">
+          <div>
+            <label for="enable-message-emails">Send me an Email when I'm messaged</label>
+            <label class="desc-label" for="enable-message-emails">Receive an email when users message you</label>
+          </div>
+          <div>
+            <input id="enable-message-emails" class="toggle-switch" type="checkbox" v-model="messageEmailsDisabled" @click="enableMessageEmails()">
+            <label for="enable-message-emails"></label>
+          </div>
+        </div>
+        <div class="toggle-switch-container">
+          <div>
+            <label for="enable-newbie-messages">Ignore Newbie Messages</label>
+            <label class="desc-label" for="enable-newbie-messages">Do not allow newbie members to message you</label>
+          </div>
+          <div>
+            <input id="enable-newbie-messages" class="toggle-switch" type="checkbox" v-model="ignoreNewbies" @click="enableNewbieMessages()">
+            <label for="enable-newbie-messages"></label>
+          </div>
+        </div>
+        <br>
+        <ignored-settings-partial :api="messagesApi" />
+      </div>
+      <div v-show="selectedTab === 'mentions'">
+        <div class="toggle-switch-container">
+          <div>
+            <label for="enable-mention-emails">Send me an Email when I'm mentioned</label>
+            <label class="desc-label" for="enable-mention-emails">Receive an email when there are thread replies which mention you</label>
+          </div>
+          <div>
+            <input id="enable-mention-emails" class="toggle-switch" type="checkbox" v-model="mentionEmailsDisabled" @click="enableMentionEmails()">
+            <label for="enable-mention-emails"></label>
+          </div>
+        </div>
+        <br>
+        <ignored-settings-partial :api="mentionsApi" />
+      </div>
     </div>
   </div>
 </template>
@@ -23,8 +61,17 @@ export default {
   name: 'ignored-settings',
   components: { IgnoredSettingsPartial },
   setup() {
-    const v = reactive({ selectedTab: 'posts' })
-    return { ...toRefs(v), usersApi, messagesApi, mentionsApi }
+    const enableMessageEmails = () => console.log('Enable Messages Email')
+    const enableNewbieMessages = () => console.log('Enable Newbie Messages')
+    const enableMentionEmails = () => console.log('Enable Mention Emails')
+
+    const v = reactive({
+      selectedTab: 'posts',
+      messageEmailsDisabled: false,
+      mentionEmailsDisabled: false,
+      ignoreNewbies: false
+    })
+    return { ...toRefs(v), enableMessageEmails, enableNewbieMessages, enableMentionEmails, usersApi, messagesApi, mentionsApi }
   }
 }
 </script>
