b9df9327cd6b5dad8a34741602e297febc0bc171
Bronson Oka <boka@slickage.com>
2021-03-23 10:20:39 -1000

    feat(threads-posted-in): add view
    (copied from other project)

---
+++ src/views/ThreadsPostedIn.vue
@@ -0,0 +1,70 @@
+<table class="threads-list">
+  <caption>Threads</caption>
+  <thead>
+    <tr>
+      <th class="subject">Threads</th>
+      <th class="views-replies">
+        <span>Replies</span>
+        <span>Views</span>
+      </th>
+      <th class="last-post">Last Post</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="threads-data" v-for="thread in PostedCtrl.threads track by thread.id">
+      <td class="subject">
+        <div class="title">
+          <div class="thread-state">
+            <svg class="is-unread" ng-if="thread.has_new_post" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+              <title></title>
+              <g id="icons">
+                <circle cx="16" cy="16" r="16" />
+              </g>
+            </svg>
+          </div>
+          <a ng-class="{bold: thread.has_new_post}" class="thread-title" ui-sref="posts.data({ slug: thread.slug })"
+            ng-bind-html="thread.title"></a>
+            <div class="thread-state-secondary">
+            <span class="thread-state-locked" ng-if="thread.locked">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                <title></title>
+                <path d="M40,21H37.5V16.48a13.5,13.5,0,0,0-27,0V21H8a2,2,0,0,0-2,2V43a2,2,0,0,0,2,2H40a2,2,0,0,0,2-2V23A2,2,0,0,0,40,21ZM15.5,16.48a8.5,8.5,0,0,1,17,0V21h-17Z"/>
+              </svg>
+            </span>
+            <span class="thread-state-hasPoll" ng-if="thread.poll">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                <path class="cls-1" d="M42,2H6A4,4,0,0,0,2,6V42a4,4,0,0,0,4,4H42a4,4,0,0,0,4-4V6A4,4,0,0,0,42,2ZM13.75,40h-6V32h6Zm9,0h-6V22h6Zm9,0h-6V27h6Zm9,0h-6V12h6Z"/>
+            </svg>
+            </span>
+          </div>
+        </div>
+
+        <div class="started-by">
+          Started by
+          <span ng-if="thread.user.deleted">deleted</span>
+          <a ng-if="!thread.user.deleted" ui-sref="profile.posts({ username: thread.user.username })"
+            ng-bind-html="thread.user.username"></a>
+          <span ng-bind="'on ' + (thread.created_at | humanDate)"></span>
+        </div>
+      </td>
+
+      <td class="views-replies">
+        <span class="replies" ng-bind="(thread.post_count - 1 | number:0) || 0"></span>
+        <span class="views" ng-bind="(thread.view_count | number:0) || 0"></span>
+      </td>
+
+      <td class="last-post">
+        <span ng-if="thread.last_deleted">deleted</span>
+        <img ng-if="!thread.last_deleted" class="avatar-small {{$webConfigs.default_avatar_shape}}"
+          ng-src="{{thread.last_post_avatar || $webConfigs.default_avatar}}" />
+        <a ng-if="!thread.last_deleted" ui-sref="profile.posts({ username: thread.last_post_username })"
+          ng-bind="thread.last_post_username"></a> posted on
+        <a ui-sref="posts.data({ slug: thread.slug, start: thread.last_post_position, '#': thread.last_post_id })"><span ng-bind="thread.last_post_created_at | humanDate"></span>.</a>
+        <span ng-if="thread.has_new_post">
+          <a
+            ui-sref="posts.data({ slug: thread.slug, start: thread.latest_unread_position, '#': thread.latest_unread_post_id })">(Last unread post)</a>
+        </span>
+      </td>
+    </tr>
+  </tbody>
+</table>
