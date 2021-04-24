53e3b5b3cc1ed1adb9856723829b283e855ff3e7
Anthony Kinsey <anthonyjk@gmail.com>
2021-04-23 14:58:50 -1000

    feat: hook up quick message, TODO: bbcode preview

--- src/components/modals/profile/QuickMessage.vue
+++ src/components/modals/profile/QuickMessage.vue
@@ -4,18 +4,27 @@
 
     <template v-slot:body>
       <form action="." class="css-form">
+        <!-- Subject -->
+        <div class="input-section">
+          <label for="subject">Subject</label>
+          <input type="text" id="subject" v-model="subject" ref="focusInput" minlength="1" maxlength="255" />
+        </div>
+
         <!-- Message -->
         <div class="input-section textarea">
           <label for="message">Message
-            ({{ message.length || 0 }}/5000)
+            ({{ body.length || 0 }}/5000)
           </label>
-          <textarea rows="10" id="message" :placeholder="`Type your message to ${user.username} here...`" v-model="message" ref="focusInput" maxlength="5000"></textarea>
+          <textarea rows="10" id="message" :placeholder="`Type your message to ${user.username} here...`" v-model="body" maxlength="5000"></textarea>
         </div>
 
         <!-- Save Button -->
-        <div class="modal-actions">
-          <button @click.prevent="sendMessage()">
+        <div class="modal-actions split-column">
+          <button @click.prevent="sendMessage()" :disabled="!subject || !body">
             Send Message
+          </button>
+          <button @click.prevent="reset()" class="outline">
+            Reset
           </button>
         </div>
         <label v-if="errorMessage" class="red centered-text">{{errorMessage}}</label>
@@ -27,6 +36,7 @@
 <script>
 import Modal from '@/components/layout/Modal.vue'
 import { reactive, toRefs, inject } from 'vue'
+import { messagesApi } from '@/api'
 
 export default {
   name: 'quick-message-modal',
@@ -36,8 +46,26 @@ export default {
   setup(props, { emit }) {
     /* Template Methods */
     const sendMessage = () => {
-      $alertStore.info(`TODO: Send \r'${v.message}'\r to ${props.user.username}`)
-      close()
+      const newMessage = {
+        receiver_ids: [ props.user.id ],
+        content: {
+          subject: v.subject,
+          body: v.body
+        }
+      }
+      messagesApi.convos.create(newMessage)
+      .then(() => $alertStore.success(`Message successfully sent to ${props.user.username}`))
+      .then(() => reset())
+      .then(() => close())
+      .catch(() => {
+        v.errorMessage = 'Error: There was an issue sending your message, please try again later'
+      })
+    }
+
+    const reset = () => {
+      v.subject = ''
+      v.body = ''
+      v.focusInput.focus()
     }
 
     const close = () => {
@@ -50,13 +78,14 @@ export default {
 
     /* Template Data */
     const v = reactive({
-      message: '',
+      subject: '',
+      body: '',
       userReactive: props.user,
       focusInput: null,
       errorMessage: ''
     })
 
-    return { ...toRefs(v), sendMessage, close }
+    return { ...toRefs(v), sendMessage, reset, close }
   }
 }
 </script>
