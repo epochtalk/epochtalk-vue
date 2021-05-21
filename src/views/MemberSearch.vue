522e17b1d951a8d2411730d1fda0133c15027e4d
Anthony Kinsey <anthonyjk@gmail.com>
2021-05-20 15:34:06 -1000

    feat: fully implement member search view, sorting now functional

--- src/views/MemberSearch.vue
+++ src/views/MemberSearch.vue
@@ -15,10 +15,10 @@
       <table width="100%">
         <thead class="members-header">
           <tr>
-            <th class="pointer" ><span :class="getSortClass('username')"></span>&nbsp;Username</th>
-            <th class="pointer" ><span :class="getSortClass('role')"></span>&nbsp;Role</th>
-            <th class="pointer"><span :class="getSortClass('post_count')"></span>&nbsp;Post Count</th>
-            <th class="pointer hide-mobile" ><span :class="getSortClass('created_at')"></span>&nbsp;Registered Date</th>
+            <th class="pointer" width="45%" @click.prevent="setSortField('username')"><span :class="getSortClass('username')"></span>&nbsp;Username</th>
+            <th class="pointer" width="20%" @click.prevent="setSortField('role')"><span :class="getSortClass('role')"></span>&nbsp;Role</th>
+            <th class="pointer" width="10%" @click.prevent="setSortField('post_count')"><span :class="getSortClass('post_count')"></span>&nbsp;Post Count</th>
+            <th class="pointer hide-mobile" width="25%" @click.prevent="setSortField('created_at')"><span :class="getSortClass('created_at')"></span>&nbsp;Registered Date</th>
           </tr>
         </thead>
         <tbody class="members">
@@ -34,7 +34,7 @@
             </td>
             <td v-html="user.role"></td>
             <td v-html="user.post_count"></td>
-            <td class="hide-mobile">{{ user.created_at }}</td>
+            <td class="hide-mobile">{{ humanDate(user.created_at, false, 'MM/DD/YYYY, h:mm A') }}</td>
           </tr>
         </tbody>
       </table>
@@ -52,6 +52,7 @@ import { reactive, toRefs, nextTick } from 'vue'
 import { usersApi } from '@/api'
 import Pagination from '@/components/layout/Pagination.vue'
 import { useRoute, useRouter } from 'vue-router'
+import humanDate from '@/composables/filters/humanDate'
 
 export default {
   name: 'About',
@@ -80,6 +81,8 @@ export default {
   setup() {
     const searchUsers = () => {
       let query = { ...$route.query, search: v.search }
+      delete query.field
+      delete query.desc
       delete query.page
       $router.replace({ name: $route.name, params: $route.params, query: query })
       v.searchInput.focus()
@@ -87,6 +90,8 @@ export default {
 
     const clearSearch = () => {
       let query = { ...$route.query }
+      delete query.field
+      delete query.desc
       delete query.search
       delete query.page
       $router.replace({ name: $route.name, params: $route.params, query: query })
@@ -94,30 +99,31 @@ export default {
       v.searchInput.focus()
     }
 
-    const setSortField = () => {
-      // // Get/Set new sort field
-      // if (newField) v.sortField = newField
-      // else newField = v.sortField
-      // // Convert desc query param to boolean
-      // let desc = $route.query.desc === 'false' ? false : true
-      // // Sort Field hasn't changed just toggle desc
-      // const defaultField = newField === 'updated_at' && !$route.query.field
-      // if (defaultField || newField === $route.query.field) desc = !desc
-      // else desc = true // Sort field changed, default to desc true
-      // // Update router to have new query params, watch on query params will update data
-      // let query = { field: newField, page: $route.query.page }
-      // if (!query.page) delete query.page // don't include page if undefined
-      // if (newField === 'updated_at') delete query.field // do not display default field in qs
-      // if (!desc) query.desc = false  // only display desc in query string when false
-      // const params = { ...$route.params, saveScrollPos: true } // save scroll pos when sorting table
-      // $router.replace({ name: $route.name, params: params, query: query })
+    const setSortField = newField => {
+      // Get/Set new sort field
+      if (newField) v.sortField = newField
+      else newField = v.sortField
+      // Convert desc query param to boolean
+      let desc = !!$route.query.desc
+      // Sort Field hasn't changed just toggle desc
+      const defaultField = newField === 'username' && !$route.query.field
+      if (defaultField || newField === $route.query.field) desc = !desc
+      else desc = true // Sort field changed, default to desc true
+      // Update router to have new query params, watch on query params will update data
+      let query = { field: newField, search: $route.query.search, page: $route.query.page }
+      if (!query.search) delete query.search
+      if (!query.page) delete query.page // don't include page if undefined
+      if (newField === 'username') delete query.field // do not display default field in qs
+      if (desc) query.desc = true  // only display desc in query string when false
+      const params = { ...$route.params, saveScrollPos: true } // save scroll pos when sorting table
+      $router.replace({ name: $route.name, params: params, query: query })
     }
 
     const getSortClass = field => {
       let sortClass = 'fa '
       const desc = v.searchData.desc
       const curField = v.searchData.field
-      const defaultField = field === 'updated_at' && !curField
+      const defaultField = field === 'username' && !curField
       if ((defaultField || curField === field) && desc) sortClass += 'fa-sort-down'
       else if ((defaultField || curField === field) && !desc) sortClass += 'fa-sort-up'
       else sortClass += 'fa-sort'
@@ -138,7 +144,7 @@ export default {
 
     nextTick(() => v.searchInput.focus())
 
-    return { ...toRefs(v), searchUsers, clearSearch, setSortField, getSortClass }
+    return { ...toRefs(v), searchUsers, clearSearch, setSortField, getSortClass, humanDate }
   }
 }
 </script>
