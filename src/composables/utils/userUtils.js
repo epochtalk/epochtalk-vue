fab4a8b8624c78ae54f2379958eb92d49b4ebe27
Anthony Kinsey <anthonyjk@gmail.com>
2021-06-01 14:34:51 -1000

    feat: add userRoleHighlight to userUtils

--- src/composables/utils/userUtils.js
+++ src/composables/utils/userUtils.js
@@ -5,3 +5,10 @@ export const usernameHighlight = color => color ? {
   padding: '0 0.125rem',
   color: '#ffffff'
 } : {}
+
+export const userRoleHighlight = color => color ? {
+  background: color,
+  border: 'none',
+  color: '#fff',
+  fontWeight: '600'
+} : {}
