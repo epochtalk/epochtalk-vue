9d16460c250a5862b0ba47e7ef9ee7069be914d9
Bronson Oka <boka@slickage.com>
2021-06-04 12:53:29 -1000

    docs(posts): remove commented code

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -670,51 +670,6 @@ export default {
         else return false
       }
       else return false
-
-      /*** translated
-      if (!pageData.write_access) { return false; }
-      if (!Session.isAuthenticated()) { return false; }
-      if (BanSvc.banStatus()) { return false; }
-      if (!Session.hasPermission('posts.delete.allow')) { return false; }
-
-      var validBypass = false;
-
-      // locked
-      if (ctrl.thread.locked) {
-        if (Session.hasPermission('posts.delete.bypass.locked.admin')) { validBypass = true; }
-        else if (Session.hasPermission('posts.delete.bypass.locked.mod')) {
-          if (Session.moderatesBoard(ctrl.thread.board_id) && Session.getPriority() < post.user.priority) { validBypass = true; }
-          // Check if mod is moderating another board's mod (which is allowed)
-          else if (Session.moderatesBoard(ctrl.thread.board_id) && (Session.getPriority() === post.user.priority && !ctrl.moderators.includes(post.user.id))) {
-            validBypass = true;
-          }
-        }
-        else if (Session.hasPermission('posts.delete.bypass.locked.priority')) {
-          if (Session.getPriority() < post.user.priority) { validBypass = true; }
-          else if (Session.hasPermission('threads.moderated.allow') && ctrl.thread.user.id === ctrl.user.id && parent.thread.moderated && ctrl.user.id !== post.user.id && Session.getPriority() <= post.user.priority) { validBypass = true; }
-        }
-      }
-      ***/
-
-      /*
-      // moderated/owner
-      if (post.user.id === ctrl.user.id) { validBypass = true; }
-      else if (ctrl.thread.moderated && ctrl.thread.user.id === ctrl.user.id && Session.hasPermission('threads.moderated.allow') && Session.hasPermission('posts.delete.bypass.owner.selfMod') && Session.getPriority() <= post.user.priority) { validBypass = true; }
-      else if (Session.hasPermission('posts.delete.bypass.owner.admin')) { validBypass = true; }
-      else if (Session.hasPermission('posts.delete.bypass.owner.mod')) {
-        if (Session.moderatesBoard(ctrl.thread.board_id) && Session.getPriority() < post.user.priority) { validBypass = true; }
-        // Check if mod is moderating another board's mod (which is allowed)
-        else if (Session.moderatesBoard(ctrl.thread.board_id) && (Session.getPriority() === post.user.priority && !ctrl.moderators.includes(post.user.id))) {
-          validBypass = true;
-        }
-      }
-      else if (Session.hasPermission('posts.delete.bypass.owner.priority')) {
-        if (Session.getPriority() < post.user.priority) { validBypass = true; }
-        else if (Session.hasPermission('threads.moderated.allow') && ctrl.thread.user.id === ctrl.user.id && parent.thread.moderated && ctrl.user.id !== post.user.id && Session.getPriority() <= post.user.priority) { validBypass = true; }
-      }
-
-      return validBypass;
-      */
     }
     const openEditThread = () => console.log('openEditThread')
     const updateThreadLock = (thread) => {
