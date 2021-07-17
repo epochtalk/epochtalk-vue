cd2015e00aa8f0fb990358d1e10a64c72bd50c3a
Anthony Kinsey <anthonyjk@gmail.com>
2021-07-16 15:40:57 -1000

    fix: refactor and clean up notifications service

--- src/composables/stores/notifications.js
+++ src/composables/stores/notifications.js
@@ -4,33 +4,9 @@ import alertStore from '@/composables/stores/alert'
 
 const store = reactive({ messages: 0, mentions: 0, mentionsList: [] })
 
-// const addAlert = (msg, type) => {
-//   // Destructure and handle SWRV object's errors in here to make Vue Component code cleaner
-//   if (typeof msg === 'object') {
-//     const error = msg.error
-//     if (error) { // detect error in swrv object
-//       switch (error.message) {
-//         case 'Network Error':
-//           msg = `${error.message}. Cannot connect to server, please try again later or contact an administrator`
-//           break
-//         default:
-//           msg = `${error.name}: ${error.message}`
-//       }
-//     }
-//     else return // no error do nothing
-//   }
-
-//   store.alerts.unshift({ id: id, type: type, message: msg })
-// }
-
-// const success = message => addAlert(message, 'success')
-// const info = message => addAlert(message, 'info')
-// const warn = message => addAlert(message, 'warning')
-// const error = message => addAlert(message, 'error')
-
 const refreshMentionsList = () => {
   return mentionsApi.page({ limit: 10 })
-  .then(mentions => Object.assign(store.mentionsList, mentions.data))
+  .then(mentions => store.mentionsList = mentions.data)
   .catch(console.log)
 }
 
@@ -64,14 +40,6 @@ const deleteMention = opts => {
     console.log(err)
   })
 }
-
-// function getMentionsList() {
-//   return mentionsList;
-// }
-
-// function getMessages() { return messages; }
-
-// function getMentions() { return mentions; }
 
 export default readonly({
   ...toRefs(store),
