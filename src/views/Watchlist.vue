034ca7869ad6fe5e542140569f87f2a47674b18f
Anthony Kinsey <anthonyjk@gmail.com>
2021-08-10 16:01:23 -1000

    feat: add last post data section links in watchlist view

--- src/views/Watchlist.vue
+++ src/views/Watchlist.vue
@@ -72,13 +72,9 @@
 
         <td class="last-post">
           <span v-if="thread.last_deleted">deleted</span>
-          <img v-if="!thread.last_deleted" class="avatar-small" :class="defaultAvatarShape"
-            :src="thread.last_post_avatar || defaultAvatar" />
-          <a v-if="!thread.last_deleted" ui-sref="profile.posts({ username: thread.last_post_username })" v-html="thread.last_post_username"></a> posted on
-          <a ui-sref="posts.data({ slug: thread.slug, start: thread.last_post_position, '#': thread.last_post_id })"><span v-html="humanDate(thread.last_post_created_at)"></span>.</a>
-          <span v-if="thread.has_new_post">
-            <a ui-sref="posts.data({ slug: thread.slug, start: thread.latest_unread_position, '#': thread.latest_unread_post_id })">(Last unread post)</a>
-          </span>
+          <router-link v-if="!thread.last_deleted" :to="{ path: '/profile/' + thread.last_post_username.toLowerCase() }"><img class="avatar-small" :class="defaultAvatar" :src="thread.last_post_avatar || defaultAvatar" @error="$event.target.src=defaultAvatar" /></router-link>
+          <router-link v-if="!thread.last_deleted" :to="{ path: '/profile/' + thread.last_post_username.toLowerCase() }" v-html="thread.last_post_username"></router-link> posted on <router-link :to="{ name: 'Posts', params: { threadSlug: thread.slug, threadId: thread.id }, query: { start: thread.last_post_position }, hash: '#' + thread.last_post_id }"><span>{{humanDate(thread.last_post_created_at)}}</span>.</router-link>
+          <router-link v-if="thread.latest_unread_post_id" :to="{ name: 'Posts', params: { threadSlug: thread.slug, threadId: thread.id }, query: { start: thread.latest_unread_position }, hash: '#' + thread.latest_unread_post_id }">(Last unread post)</router-link>
         </td>
       </tr>
     </tbody>
