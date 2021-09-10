1b903a4c0cf47ad8e12bef582e7aaa8898ec1ba4
Anthony Kinsey <anthonyjk@gmail.com>
2021-09-09 15:28:15 -1000

    feat: implement recursive component for trust depth

---
+++ src/components/trust/TrustHierarchyPartial.vue
@@ -0,0 +1,19 @@
+<template>
+  <li v-for="user in trustTree" :key="user">
+    <span v-if="user.type === 0">{{ user.username_trusted }}</span>
+    <span v-if="user.type === 1"><s>{{ user.username_trusted }}</s></span>
+    <ul v-if="user.trusted">
+      <trust-hierarchy-partial :trust-tree="user.trusted" />
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'trust-hierarchy-partial',
+  props: ['trustTree'],
+  setup() {
+    return {}
+  }
+}
+</script>
