9f9e68f6c8bfcedc8d5217c7351ad4a53ee723cb
Bronson Oka <boka@slickage.com>
2021-10-14 09:59:12 -1000

    refactor(poll-creator): poll -> pollCopy

--- src/components/polls/PollCreator.vue
+++ src/components/polls/PollCreator.vue
@@ -4,19 +4,19 @@
       <span class="section-header">Create a Poll</span>
       <!-- Poll Question -->
       <label for="question" class="poll-question">Question:</label>
-      <input type="text" id="question" v-model="poll.question" maxlength="255">
+      <input type="text" id="question" v-model="pollCopy.question" maxlength="255">
 
       <!-- Poll Answers -->
       <label for="answer-1" class="poll-answers">Answers:</label>
-      <div class="answer-row" v-for="(answer, index) in poll.answers" :key="index">
+      <div class="answer-row" v-for="(answer, index) in pollCopy.answers" :key="index">
         <div class="field-inline-action">
-          <input type="text" :id="'answer-' + index" v-model="poll.answers[index]" maxlength="255">
+          <input type="text" :id="'answer-' + index" v-model="pollCopy.answers[index]" maxlength="255">
           <button v-if="index > 1" tabindex="-1" @click="removePollAnswer(index)" class="inline-action secondary small"><i class="fa fa-minus-circle"></i></button>
         </div>
       </div>
       <!-- Add Answer Button -->
       <div class="actions">
-        <button @click="addPollAnswer()" :disabled="poll.answers.length > 19" class="secondary small">Add an Answer</button>
+        <button @click="addPollAnswer()" :disabled="pollCopy.answers.length > 19" class="secondary small">Add an Answer</button>
       </div>
     </div>
 
@@ -24,36 +24,36 @@
       <span class="section-header">Options:</span >
 
       <div class="checkbox-group">
-        <input type="checkbox" id="pollVote" v-model="poll.change_vote">
+        <input type="checkbox" id="pollVote" v-model="pollCopy.change_vote">
         <label for="pollVote">Allow user to change vote</label>
       </div>
 
       <div class="poll-options-expiration">
         <label for="pollExpirationDate" class="fieldgroup-container">
-          <span class="fieldgroup-label">Poll expires at -- {{humanDate(poll.expiration) || '(No Expiration)'}}</span>
+          <span class="fieldgroup-label">Poll expires at -- {{humanDate(pollCopy.expiration) || '(No Expiration)'}}</span>
           <div class="fieldgroup">
-            <input type="date" id="pollExpirationDate" v-model="poll.expiration_date" @change="calcExpiration()">
-            <input type="time" v-model="poll.expiration_time" v-change="calcExpiration()">
+            <input type="date" id="pollExpirationDate" v-model="pollCopy.expiration_date" @change="calcExpiration()">
+            <input type="time" v-model="pollCopy.expiration_time" v-change="calcExpiration()">
           </div>
         </label>
       </div>
 
       <div class="poll-options-answers">
         <label for="pollMaxVote">Maximum answers per vote:</label>
-        <input type="number" id="pollMaxVote" min="1" :max="poll.answers.length" v-model="poll.max_answers">
+        <input type="number" id="pollMaxVote" min="1" :max="pollCopy.answers.length" v-model="pollCopy.max_answers">
 
         <div class="">
           <label>Show poll results: </label>
           <label for="displayAlways">
-            <input type="radio" id="displayAlways" v-model="poll.display_mode" value="always">
+            <input type="radio" id="displayAlways" v-model="pollCopy.display_mode" value="always">
             Always shown
           </label>
           <label for="displayVoted">
-            <input type="radio" id="displayVoted" v-model="poll.display_mode" value="voted">
+            <input type="radio" id="displayVoted" v-model="pollCopy.display_mode" value="voted">
             After voting
           </label>
           <label for="displayExpired">
-            <input type="radio" id="displayExpired" v-model="poll.display_mode" value="expired" :disabled="!poll.expiration">
+            <input type="radio" id="displayExpired" v-model="pollCopy.display_mode" value="expired" :disabled="!pollCopy.expiration">
             After expiration
           </label>
         </div>
