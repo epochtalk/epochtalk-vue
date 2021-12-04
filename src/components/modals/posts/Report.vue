7e725c5a9b639a4dfa816565eb995b0d9beb6a80
Bronson Oka <boka@slickage.com>
2021-12-03 14:29:14 -1000

    docs(todos): remove todo
    default value already exists

--- src/components/modals/posts/Report.vue
+++ src/components/modals/posts/Report.vue
@@ -7,7 +7,6 @@
       <form action="." class="css-form" @submit.prevent="submitReport()">
         <h3 class="thin-underline">Purge Post</h3>
         <p v-if="canReportPosts && canReportUsers && !selectedPost.reported">Which would you like to report?</p>
-        <!-- TODO(boka): default radio button choice -->
         <input v-if="canReportUsers" type="radio" ref="focusInput" name="reportPost" v-model="offendingId" :value="selectedPost.user.id" id="reportUser" required checked="true"><label v-if="canReportUsers" for="reportUser">{{selectedPost.user.username}}</label>
         <input v-if="canReportPosts" type="radio" name="reportPost" v-model="offendingId" :value="selectedPost.id" id="reportPost" required><label v-if="canReportPosts" for="reportPost">{{selectedPost.user.username}}'s Post</label>
         <label>Reason for Report</label>
