3e73d1546a10a180abae6ae419999c4a5cbd1b02
Anthony Kinsey <kkid@Anthonys-MacBook-Pro.local>
2022-01-11 15:08:52 -1000

    feat: add message when no boards are present

--- src/views/Boards.vue
+++ src/views/Boards.vue
@@ -10,7 +10,9 @@
       <router-link :to="{ name: 'Watchlist' }" class="button">Watchlist</router-link>
       <router-link :to="{ name: 'ThreadsPostedIn' }" class="button">Threads Posted In</router-link>
     </div>
-
+    <div v-if="!boardData?.data?.boards?.length">
+      <h4 class="centered-text">There are currently no boards, an administrator must add boards before threads can be created</h4>
+    </div>
     <div v-if="boardData.data">
       <div class="category" v-for="cat in boardData.data.boards" :key="cat.id">
         <!-- Category Title -->
