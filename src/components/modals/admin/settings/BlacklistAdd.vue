617afa486f0951aa14300e2d56e30ba12130c62a
Anthony Kinsey <anthonyjk@gmail.com>
2022-03-09 11:29:07 -1000

    feat: modify blacklist add modal to work for add/edit/delete, todo rename

--- src/components/modals/admin/settings/BlacklistAdd.vue
+++ src/components/modals/admin/settings/BlacklistAdd.vue
@@ -1,21 +1,25 @@
 <template>
   <modal :name="$options.name" :show="show" @close="close()" :focusInput="focusInput">
-    <template v-slot:header>Add IP Blacklist Rule</template>
+    <template v-slot:header>
+      <span v-if="add">Add IP Blacklist Rule</span>
+      <span v-if="edit">Edit IP Blacklist Rule</span>
+      <span v-if="remove">Delete IP Blacklist Rule</span>
+    </template>
 
     <template v-slot:body>
         <form class="css-form">
           <label>Enter Rule Name
-            <input type="text" placeholder="Rule Name" v-model="rule.note" :disabled="addSubmitted" ref="focusInput" required />
+            <input type="text" placeholder="Rule Name" v-model="rule.note" :disabled="requestSubmitted || remove" ref="focusInput" required />
           </label>
 
           <label for="ruleType">Select Rule Type:</label>
-          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="0" id="single" :disabled="addSubmitted" required />
+          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="0" id="single" :disabled="requestSubmitted || remove" required />
           <label for="single">Single IP</label>
 
-          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="1" id="range" :disabled="addSubmitted" required />
+          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="1" id="range" :disabled="requestSubmitted || remove" required />
           <label for="range">IP Range</label>
 
-          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="2" id="wildcard" :disabled="addSubmitted" required />
+          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="2" id="wildcard" :disabled="requestSubmitted || remove" required />
           <label for="wildcard">Wildcard IP</label>
 
           <div v-if="rule.type === 0" class="single-ip">
@@ -28,10 +32,10 @@
             <label>Enter a range of IPV4 or IPV6 addresses</label>
             <div class="col">
               <div>
-                <input type="text" v-model="rule.start" placeholder="Start Ex: 203.0.113.0 or 2001:0db8:0000:0000:0000:0000:0000:0000" ng-pattern="ipRegex" :disabled="addSubmitted" required />
+                <input type="text" v-model="rule.start" placeholder="Start Ex: 203.0.113.0 or 2001:0db8:0000:0000:0000:0000:0000:0000" ng-pattern="ipRegex" :disabled="requestSubmitted || remove" required />
               </div>
               <div>
-                <input type="text" v-model="rule.end" placeholder="End Ex: 203.0.113.255 or 2001:0db8:ffff:ffff:ffff:ffff:ffff:ffff" ng-pattern="ipRegex" :disabled="addSubmitted" required />
+                <input type="text" v-model="rule.end" placeholder="End Ex: 203.0.113.255 or 2001:0db8:ffff:ffff:ffff:ffff:ffff:ffff" ng-pattern="ipRegex" :disabled="requestSubmitted || remove" required />
               </div>
             </div>
           </div>
@@ -39,21 +43,20 @@
           <div v-if="rule.type === 2">
             <label>Enter an IPV4 wildcard</label>
             <div class="wildcard col">
-              <input type="text" v-model="rule.blockOne" ng-pattern="blockRegex" placeholder="203" maxlength="3" :disabled="addSubmitted" required />
-              <input type="text" v-model="rule.blockTwo" :disabled="addSubmitted" ng-pattern="blockWildcardRegex" placeholder="0" maxlength="3" required />
-              <input type="text" v-model="rule.blockThree" :disabled="addSubmitted" ng-pattern="blockWildcardRegex" placeholder="113" maxlength="3" required />
-              <input type="text" v-model="rule.blockFour" ng-pattern="blockWildcardRegex" placeholder="*" maxlength="3" :disabled="addSubmitted" required />
+              <input type="text" v-model="rule.blockOne" ng-pattern="blockRegex" placeholder="203" maxlength="3" :disabled="requestSubmitted || remove" required />
+              <input type="text" v-model="rule.blockTwo" :disabled="requestSubmitted || remove" ng-pattern="blockWildcardRegex" placeholder="0" maxlength="3" required />
+              <input type="text" v-model="rule.blockThree" :disabled="requestSubmitted || remove" ng-pattern="blockWildcardRegex" placeholder="113" maxlength="3" required />
+              <input type="text" v-model="rule.blockFour" ng-pattern="blockWildcardRegex" placeholder="*" maxlength="3" :disabled="requestSubmitted || remove" required />
             </div>
           </div>
 
           <div class="col">
             <div>
-              <button class="fill-row" @click.prevent="addRule()"
-              :disabled="addSubmitted" v-html="saveRuleBtnLabel"></button>
+              <button class="fill-row" v-if="add" @click.prevent="modifyRule(true)" :disabled="requestSubmitted" v-html="saveContinueBtnLabel"></button>
+              <button class="fill-row negative" v-if="edit || remove" @click.prevent="close()" :disabled="requestSubmitted">Cancel</button>
             </div>
             <div>
-              <button class="fill-row" @click.prevent="addRule(true)"
-              :disabled="addSubmitted" v-html="saveContinueBtnLabel"></button>
+              <button class="fill-row" @click.prevent="modifyRule()" :disabled="requestSubmitted" v-html="saveRuleBtnLabel"></button>
             </div>
           </div>
         <label v-if="errorMessage" class="red centered-text">{{errorMessage}}</label>
@@ -64,12 +67,12 @@
 
 <script>
 import Modal from '@/components/layout/Modal.vue'
-import { reactive, toRefs, inject } from 'vue'
+import { reactive, toRefs, inject, watch } from 'vue'
 import { adminApi } from '@/api'
 
 export default {
   name: 'delete-message-modal',
-  props: ['show', 'messageId'],
+  props: ['show', 'messageId', 'add', 'edit', 'remove', 'selected'],
   emits: ['close', 'success'],
   components: { Modal },
   setup(props, { emit }) {
@@ -99,34 +102,63 @@ export default {
       return formattedRule
     }
 
-    const resetForm = () => {
-      v.errorMessage = null
-      v.rule = { type: 0 }
-      v.addSubmitted = false
-      v.saveRuleBtnLabel = 'Save'
+    const resetForm = saveError => {
+      v.errorMessage = saveError ? v.errorMessage : null
+      v.rule = saveError ? v.rule : { type: 0 }
+      v.requestSubmitted = false
+      v.saveRuleBtnLabel = props.remove ? 'Confirm Delete' : 'Save'
       v.saveContinueBtnLabel = 'Save and Continue'
     }
 
-    const addRule = saveAndContinue => {
+    const modifyRule = saveAndContinue => {
       v.errorMessage = null
-      v.addSubmitted = true
+      v.requestSubmitted = true
       if (saveAndContinue) v.saveContinueBtnLabel = 'Loading...'
       else v.saveRuleBtnLabel = 'Loading...'
-      let ruleToAdd = formatIPRule(v.rule)
-      if (!ruleToAdd) {
+      let requestRule = formatIPRule(v.rule)
+      if (!requestRule) {
         resetForm()
         return
       }
-      adminApi.blacklist.add(ruleToAdd)
-      .then(() => {
-        emit('success')
-        $alertStore.success(`Successfully added blacklist rule!`)
-      })
-      .catch(() => v.errorMessage = `There was an error adding blacklist rule.`)
-      .finally(() => {
-        if (!v.errorMessage && saveAndContinue) resetForm()
-        else if (!v.errorMessage) close()
-      })
+       if (props.add) {
+        adminApi.blacklist.add(requestRule)
+        .then(() => {
+          emit('success')
+          $alertStore.success(`Successfully added blacklist rule!`)
+        })
+        .catch(() => v.errorMessage = `There was an error adding blacklist rule.`)
+        .finally(() => {
+          if (!v.errorMessage && saveAndContinue) resetForm()
+          else if (!v.errorMessage) close()
+          else resetForm(true)
+        })
+       }
+       else if (props.edit) {
+        adminApi.blacklist.update(requestRule)
+        .then(() => {
+          emit('success')
+          $alertStore.success(`Successfully updated blacklist rule!`)
+        })
+        .catch(() => v.errorMessage = `There was an error updating blacklist rule.`)
+        .finally(() => {
+          if (!v.errorMessage && saveAndContinue) resetForm()
+          else if (!v.errorMessage) close()
+          else resetForm(true)
+        })
+       }
+       else if (props.remove) {
+        adminApi.blacklist.delete(props.selected.id)
+        .then(() => {
+          emit('success')
+          $alertStore.success(`Successfully deleted blacklist rule!`)
+        })
+        .catch(() => v.errorMessage = `There was an error deleting blacklist rule.`)
+        .finally(() => {
+          if (!v.errorMessage && saveAndContinue) resetForm()
+          else if (!v.errorMessage) close()
+          else resetForm(true)
+        })
+       }
     }
 
     const close = () => {
@@ -140,14 +172,44 @@ export default {
     /* Template Data */
     const v = reactive({
       focusInput: null,
-      rule: { type: 0 },
-      saveRuleBtnLabel: 'Save',
+      rule:  { type: 0 } ,
+      saveRuleBtnLabel: props.remove ? 'Confirm Delete' : 'Save',
       saveContinueBtnLabel: 'Save and Continue',
-      addSubmitted: false,
+      requestSubmitted: false,
       errorMessage: ''
     })
 
-    return { ...toRefs(v), addRule, close }
+    watch(() => props.show, () => {
+      v.saveRuleBtnLabel = 'Save'
+      if (props.add) v.rule = { type: 0 }
+      else if (props.edit) {
+        v.rule = { ...props.selected, type: 1 }
+        if (v.rule.ip_data.indexOf('*') > -1) { // wildcard type 2
+          let blocks = v.rule.ip_data.split('.')
+          v.rule.type = 2
+          v.rule.blockOne = blocks[0]
+          v.rule.blockTwo = blocks[1]
+          v.rule.blockThree = blocks[2]
+          v.rule.blockFour = blocks[3]
+        }
+        else if (v.rule.ip_data.indexOf('-') > -1) { // range type 1
+          v.rule.type = 1
+          let range = v.rule.ip_data.split('-')
+          v.rule.start = range[0]
+          v.rule.end = range[1]
+        }
+        else { // standard ip
+          v.rule.type = 0
+          v.rule.ip = v.rule.ip_data
+        }
+      }
+      else if (props.remove) {
+        v.rule = { ...props.selected, type: 2 }
+        v.saveRuleBtnLabel = 'Confirm Delete'
+      }
+    })
+
+    return { ...toRefs(v), modifyRule, close }
   }
 }
 </script>
