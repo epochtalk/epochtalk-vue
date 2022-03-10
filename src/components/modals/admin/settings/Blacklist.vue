40d85e74ae00e52aae2da0d09b22825872d03767
Anthony Kinsey <anthonyjk@gmail.com>
2022-03-10 11:35:12 -1000

    feat: port regex for blacklist input modal

--- src/components/modals/admin/settings/Blacklist.vue
+++ src/components/modals/admin/settings/Blacklist.vue
@@ -13,18 +13,18 @@
           </label>
 
           <label for="ruleType">Select Rule Type:</label>
-          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="0" id="single" :disabled="requestSubmitted || remove" required />
+          <input type="radio" class="pointer" name="ruleType" @click="formValid = rule.note && ipRegex.test(rule.ip)" v-model="rule.type" :value="0" id="single" :disabled="requestSubmitted || remove" required />
           <label for="single">Single IP</label>
 
-          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="1" id="range" :disabled="requestSubmitted || remove" required />
+          <input type="radio" class="pointer" name="ruleType" @click="formValid = rule.note && ipRegex.test(rule.start) && ipRegex.test(rule.end)" v-model="rule.type" :value="1" id="range" :disabled="requestSubmitted || remove" required />
           <label for="range">IP Range</label>
 
-          <input type="radio" class="pointer" name="ruleType" v-model="rule.type" :value="2" id="wildcard" :disabled="requestSubmitted || remove" required />
+          <input type="radio" class="pointer" name="ruleType" @click="formValid = rule.note && blockIpRegex.test(rule.blockOne) && blockIpWildcardRegex.test(rule.blockTwo) && blockIpWildcardRegex.test(rule.blockThree) && blockIpWildcardRegex.test(rule.blockFour)" v-model="rule.type" :value="2" id="wildcard" :disabled="requestSubmitted || remove" required />
           <label for="wildcard">Wildcard IP</label>
 
           <div v-if="rule.type === 0" class="single-ip">
             <label>Enter a single IPV4 or IPV6 address
-              <input type="text" v-model="rule.ip" placeholder="Ex: 203.0.113.15 or 2001:0db8:0f61:a1ff:0000:0000:0000:0080" ng-pattern="ipRegex" required />
+              <input type="text" v-model="rule.ip" placeholder="Ex: 203.0.113.15 or 2001:0db8:0f61:a1ff:0000:0000:0000:0080" required />
             </label>
           </div>
 
@@ -32,10 +32,10 @@
             <label>Enter a range of IPV4 or IPV6 addresses</label>
             <div class="col">
               <div>
-                <input type="text" v-model="rule.start" placeholder="Start Ex: 203.0.113.0 or 2001:0db8:0000:0000:0000:0000:0000:0000" ng-pattern="ipRegex" :disabled="requestSubmitted || remove" required />
+                <input type="text" v-model="rule.start" placeholder="Start Ex: 203.0.113.0 or 2001:0db8:0000:0000:0000:0000:0000:0000" :disabled="requestSubmitted || remove" required />
               </div>
               <div>
-                <input type="text" v-model="rule.end" placeholder="End Ex: 203.0.113.255 or 2001:0db8:ffff:ffff:ffff:ffff:ffff:ffff" ng-pattern="ipRegex" :disabled="requestSubmitted || remove" required />
+                <input type="text" v-model="rule.end" placeholder="End Ex: 203.0.113.255 or 2001:0db8:ffff:ffff:ffff:ffff:ffff:ffff" :disabled="requestSubmitted || remove" required />
               </div>
             </div>
           </div>
@@ -43,20 +43,20 @@
           <div v-if="rule.type === 2">
             <label>Enter an IPV4 wildcard</label>
             <div class="wildcard col">
-              <input type="text" v-model="rule.blockOne" ng-pattern="blockRegex" placeholder="203" maxlength="3" :disabled="requestSubmitted || remove" required />
-              <input type="text" v-model="rule.blockTwo" :disabled="requestSubmitted || remove" ng-pattern="blockWildcardRegex" placeholder="0" maxlength="3" required />
-              <input type="text" v-model="rule.blockThree" :disabled="requestSubmitted || remove" ng-pattern="blockWildcardRegex" placeholder="113" maxlength="3" required />
-              <input type="text" v-model="rule.blockFour" ng-pattern="blockWildcardRegex" placeholder="*" maxlength="3" :disabled="requestSubmitted || remove" required />
+              <input type="text" v-model="rule.blockOne" placeholder="203" maxlength="3" :disabled="requestSubmitted || remove" required />
+              <input type="text" v-model="rule.blockTwo" :disabled="requestSubmitted || remove" placeholder="0" maxlength="3" required />
+              <input type="text" v-model="rule.blockThree" :disabled="requestSubmitted || remove" placeholder="113" maxlength="3" required />
+              <input type="text" v-model="rule.blockFour" placeholder="*" maxlength="3" :disabled="requestSubmitted || remove" required />
             </div>
           </div>
 
           <div class="col">
             <div>
-              <button class="fill-row" v-if="add" @click.prevent="modifyRule(true)" :disabled="requestSubmitted" v-html="saveContinueBtnLabel"></button>
+              <button class="fill-row" v-if="add" @click.prevent="modifyRule(true)" :disabled="requestSubmitted || !formValid" v-html="saveContinueBtnLabel"></button>
               <button class="fill-row negative" v-if="edit || remove" @click.prevent="close()" :disabled="requestSubmitted">Cancel</button>
             </div>
             <div>
-              <button class="fill-row" @click.prevent="modifyRule()" :disabled="requestSubmitted" v-html="saveRuleBtnLabel"></button>
+              <button class="fill-row" @click.prevent="modifyRule()" :disabled="requestSubmitted || (!formValid && !remove)" v-html="saveRuleBtnLabel"></button>
             </div>
           </div>
         <label v-if="errorMessage" class="red centered-text">{{errorMessage}}</label>
@@ -69,6 +69,8 @@
 import Modal from '@/components/layout/Modal.vue'
 import { reactive, toRefs, inject, watch } from 'vue'
 import { adminApi } from '@/api'
+import { ipRegex, blockIpRegex, blockIpWildcardRegex } from '@/composables/utils/globalRegex'
+import { debounce } from 'lodash'
 
 export default {
   name: 'delete-message-modal',
@@ -176,6 +178,7 @@ export default {
       saveRuleBtnLabel: props.remove ? 'Confirm Delete' : 'Save',
       saveContinueBtnLabel: 'Save and Continue',
       requestSubmitted: false,
+      formValid: false,
       errorMessage: ''
     })
 
@@ -209,7 +212,14 @@ export default {
       }
     })
 
-    return { ...toRefs(v), modifyRule, close }
+    watch(() => v.rule.ip, debounce(async val => v.formValid = v.rule.note && v.rule.type === 0 && ipRegex.test(val)))
+    watch(() => v.rule.start, debounce(async () => v.formValid = v.rule.note && v.rule.type === 1 && ipRegex.test(v.rule.start) && ipRegex.test(v.rule.end)))
+    watch(() => v.rule.end, debounce(async () => v.formValid = v.rule.note && v.rule.type === 1 && ipRegex.test(v.rule.start) && ipRegex.test(v.rule.end)))
+    watch(() => v.rule.blockOne, debounce(async () => v.formValid = v.rule.note && v.rule.type === 2 && blockIpRegex.test(v.rule.blockOne) && blockIpWildcardRegex.test(v.rule.blockTwo) && blockIpWildcardRegex.test(v.rule.blockThree) && blockIpWildcardRegex.test(v.rule.blockFour)))
+    watch(() => v.rule.blockTwo, debounce(async () => v.formValid = v.rule.note && v.rule.type === 2 && blockIpRegex.test(v.rule.blockOne) && blockIpWildcardRegex.test(v.rule.blockTwo) && blockIpWildcardRegex.test(v.rule.blockThree) && blockIpWildcardRegex.test(v.rule.blockFour)))
+    watch(() => v.rule.blockThree, debounce(async () => v.formValid = v.rule.note && v.rule.type === 2 && blockIpRegex.test(v.rule.blockOne) && blockIpWildcardRegex.test(v.rule.blockTwo) && blockIpWildcardRegex.test(v.rule.blockThree) && blockIpWildcardRegex.test(v.rule.blockFour)))
+    watch(() => v.rule.blockFour, debounce(async () => v.formValid = v.rule.note && v.rule.type === 2 && blockIpRegex.test(v.rule.blockOne) && blockIpWildcardRegex.test(v.rule.blockTwo) && blockIpWildcardRegex.test(v.rule.blockThree) && blockIpWildcardRegex.test(v.rule.blockFour)))
+    return { ...toRefs(v), modifyRule, close, ipRegex, blockIpRegex, blockIpWildcardRegex }
   }
 }
 </script>
