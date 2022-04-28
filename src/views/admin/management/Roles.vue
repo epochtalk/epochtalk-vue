3a230fae8639a4275436f9cbb89c6963cf743ce1
Anthony Kinsey <anthonyjk@gmail.com>
2022-04-28 11:39:46 -1000

    feat: fix layout of roles management

--- src/views/admin/management/Roles.vue
+++ src/views/admin/management/Roles.vue
@@ -9,65 +9,43 @@
         <input class="nested-input" v-model="newRole.name" type="text" id="add-role" placeholder="Enter new role name" @keydown="$event.which === 13 && showRole()" />
       </div>
     </div>
-      <draggable v-model="roles" @sort="reprioritizeRoles" item-key="id">
-        <template #header>
-          <div class="role-header">
-            <span></span>
-            <span>Role Name</span>
-            <span>Description</span>
-            <span>Actions</span>
-          </div>
-        </template>
-        <template #item="{element}">
-          <div class="role-row pointer" @click="selectRole(element)">
-            <span class="centered-text"><i class="fa fa-align-justify" /></span>
-            <span v-html="element.name"></span>
-            <span v-html="element.description"></span>
-            <span>
-              <a href="#" v-if="controlAccess.update.allow" @click.prevent="showRole(element)">Edit</a>
-              <span v-if="!controlAccess.update.allow">Edit</span>
-              &nbsp;&nbsp;
-              <a v-if="element.id === element.lookup && controlAccess.remove.allow" href="#" @click.prevent="showRemoveRole(element)">Remove</a>
-              <a v-if="element.id !== element.lookup" href="#" @click.prevent="showResetRole(element)">Reset</a>
-            </span>
-          </div>
-        </template>
-        <template #footer>
-          <div class="actions">
-            <div v-if="controlAccess.reprioritize.allow" class="right"><button @click="resetPriority()">Reset Priority</button></div>
-            <div v-if="controlAccess.reprioritize.allow" class="right"><button @click="savePriority()">Save Priority</button>&nbsp;&nbsp;&nbsp;</div>
-          </div>
-        </template>
-      </draggable>
-<!--     <table sv-root sv-on-sort="AdminManagementCtrl.reprioritizeRoles()" sv-part="AdminManagementCtrl.roles" width="100%" class="admin-content-selectable underlined">
-      <thead>
-        <th></th>
-        <th>Role Name</th>
-        <th>Description</th>
-        <th>Actions</th>
-      </thead>
-      <tbody ng-repeat="role in AdminManagementCtrl.roles">
-        <tr ng-class="{ 'active-row' : AdminManagementCtrl.selectedRole.id === role.id }" class="selectable-row pointer" ng-click="AdminManagementCtrl.selectRole(role)" sv-element>
-          <td><i class="fa fa-align-justify" /></td>
-          <td ng-bind-html="role.name"></td>
-          <td ng-bind-html="role.description"></td>
-          <td>
-            <a ng-href="#" ng-if="AdminManagementCtrl.controlAccess.update.allow" ng-click="AdminManagementCtrl.showRole(role)">Edit</a>
-            <span ng-if="!AdminManagementCtrl.controlAccess.update.allow">Edit</span>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <a ng-if="role.id === role.lookup && AdminManagementCtrl.controlAccess.remove.allow" ng-href="#" ng-click="AdminManagementCtrl.showRemoveRole(role)">Remove</a>
-            <a ng-if="role.id !== role.lookup" ng-href="#" ng-click="AdminManagementCtrl.showResetRole(role)">Reset</a>
-          </td>
-        </tr>
-      </tbody>
-    </table> -->
+    <draggable v-model="roles" @sort="reprioritizeRoles" item-key="id">
+      <template #header>
+        <div class="role-table-header">
+          <span></span>
+          <span>Role Name</span>
+          <span>Description</span>
+          <span>Actions</span>
+        </div>
+      </template>
+      <template #item="{element}">
+        <div :class="{ 'selected-row' : selectedRole.id === element.id }" class="role-table-row pointer" @click="selectRole(element)">
+          <span class="centered-text"><i class="fa fa-align-justify" /></span>
+          <span v-html="element.name"></span>
+          <span v-html="element.description"></span>
+          <span>
+            <a href="#" v-if="controlAccess.update.allow" @click.prevent="showRole(element)">Edit</a>
+            <span v-if="!controlAccess.update.allow">Edit</span>
+            &nbsp;&nbsp;
+            <a v-if="element.id === element.lookup && controlAccess.remove.allow" href="#" @click.prevent="showRemoveRole(element)">Remove</a>
+            <a v-if="element.id !== element.lookup" href="#" @click.prevent="showResetRole(element)">Reset</a>
+          </span>
+        </div>
+      </template>
+      <template #footer>
+        <div class="actions">
+          <div v-if="controlAccess.reprioritize.allow" class="right"><button @click="resetPriority()">Reset Priority</button></div>
+          <div v-if="controlAccess.reprioritize.allow" class="right"><button @click="savePriority()">Save Priority</button>&nbsp;&nbsp;&nbsp;</div>
+        </div>
+      </template>
+    </draggable>
   </div>
 
   <div v-if="selectedRole !== null && !selectedRole.message">
     <br />
     <h3 class="role-header">{{selectedRole.name}} ({{userData.count}} users)</h3>
     <div class="add-users">
-      <a v-if="userData.count > 0" @click="showFilterUsers = !showFilterUsers">
+      <a href="#" v-if="userData.count > 0" @click.prevent="showFilterUsers = !showFilterUsers">
         <i class="fa fa-search"></i> Filter Users
       </a>
     </div>
@@ -89,9 +67,10 @@
 <!--       <tags-input min-length="1" placeholder="Type username to add" add-from-autocomplete-only="true" replace-spaces-with-dashes="false" ng-model="AdminManagementCtrl.usersToAdd">
         <auto-complete min-length="1" debounce-delay="250" source="AdminManagementCtrl.loadTags($query)"></auto-complete>
       </tags-input> -->
+      <input type="text" />
       <div class="button-container">
-        <button class="half-button" @click="usersToAdd = null" :disabled="!usersToAdd || usersToAdd.length < 1">Clear</button>
-        <button class="half-button" @click="addUsers()" :disabled="!usersToAdd || usersToAdd.length < 1">Add User(s)</button>
+        <button @click="usersToAdd = null" :disabled="!usersToAdd || usersToAdd.length < 1">Clear</button>
+        <button @click="addUsers()" :disabled="!usersToAdd || usersToAdd.length < 1">Add User(s)</button>
       </div>
     </div>
     <div v-if="userData.count > 0">
@@ -242,18 +221,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .role-header {
+  .role-header { display: inline-block; }
+  .roles.user-search, .roles.add-role { width: 100%; }
+  .add-users { float: right;  line-height: 1.5rem; }
+  .button-container {
+    display: grid;
+    grid-column-gap: .5rem;
+    grid-template-columns: 1fr 1fr;
+  }
+  .selected-row {
+    background-color: $color-primary;
+    color: $base-background-color;
+  }
+  .role-table-header {
     font-size: 0.75rem;
     border-bottom: 1px solid $border-color-alt;
     font-weight: 400;
     color: $secondary-font-color;
   }
-  .role-row {
+  .role-table-row {
     font-size: 0.875rem;
     padding: .5rem 0;
     border-bottom: 1px solid $border-color-alt;
   }
-  .role-header, .role-row {
+  .role-table-header, .role-table-row {
     display: grid;
     grid-template-columns: 2rem 10rem 1fr 6rem;
   }
