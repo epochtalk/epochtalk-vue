8f09c3e4c8807f9255fa7b9fb93c175ec36596a2
Anthony Kinsey <anthony@slickage.com>
2024-05-10 08:24:55 -1000

    fix(tz-issues): resolve timezone display issues, todo: clean up code

--- src/composables/filters/humanDate.js
+++ src/composables/filters/humanDate.js
@@ -9,24 +9,18 @@ export default (dateStr, hideTime, customFormat) => {
   if (!dateStr) return dateStr
 
   let storedTz = $prefs.data ? $prefs.data.timezone_offset : null
-  let timezoneOffset = storedTz ? storedTz.sign + (Number(storedTz.hours * 60) + Number(storedTz.minutes)) : null
+  let timezoneOffset = storedTz ? Number(storedTz.sign + (Number(storedTz.hours * 60) + Number(storedTz.minutes))) : null
 
   let result
   let now = new Date()
   let maxDate = new Date(8640000000000000)
-  let date = new Date(dateStr)
+  let date = new Date(dateStr + 'Z')
   let isToday = now.toDateString() === date.toDateString()
   let isThisYear = now.getYear() === date.getYear()
   let isMaxDate = maxDate.getTime() === date.getTime()
 
-  // TODO(akinsey): load users timezone setting from saved preferences
-  // otherwise guess timzone.
-  console.log("tz offset:", timezoneOffset)
-  console.log("raw date str:", dateStr)
-  console.log("date:", date, date.getTime())
-  console.log("dayjs:", dayjs(date.getTime()))
-  console.log("dayjs:", dayjs(date.getTime()).utcOffset(timezoneOffset))
-  let convertedDate = dayjs.utc(dateStr).tz(dayjs.tz.guess())
+  // if timezoneOffset is set in prefs, apply it, otherwise guess timzone
+  let convertedDate = timezoneOffset ? dayjs.utc(date).utcOffset(timezoneOffset) : dayjs.utc(dateStr).tz(dayjs.tz.guess())
 
   if (hideTime) {
     if (isToday) { result = 'Today' }
@@ -35,7 +29,6 @@ export default (dateStr, hideTime, customFormat) => {
   }
   else if (customFormat) result = convertedDate.format(customFormat)
   else {
-    // TODO(akinsey): properly apply timezone fetched from prefs
     if (isToday) { result = 'Today at ' +  convertedDate.format('h:mm A') }
     else if (isThisYear) { result = convertedDate.format('MMMM D [at] h:mm A') }
     else { result = convertedDate.format('MMM D, YYYY [at] h:mm A') }
