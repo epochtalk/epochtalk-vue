56b2a3855de865ac4daf174a3b41ec05b192bbd2
unenglishable <unenglishable@gmail.com>
2024-05-10 09:27:53 -1000

    fix(filters/humanDate): use only dayjs
    use lodash get to parse nested objects
    
    use hours for timezone offset instead of minutes
    (handles case where offset is +/-15, which would have loaded as hours)
    
    check if timezone offset is valid before setting time and running comparisons
    
    run comparisons (today, this year) in user's local timezone or offset
    
    refactor variable names to read a little better
    
    handle edge cases:
    
      no preferences
    
      no timezone offset preferences
    
      offset is 0
    
      offset is +/-15

--- src/composables/filters/humanDate.js
+++ src/composables/filters/humanDate.js
@@ -1,4 +1,5 @@
 import * as dayjs from 'dayjs'
+import { get } from 'lodash'
 import timezone from 'dayjs/plugin/timezone'
 import localStorageCache from '@/composables/utils/localStorageCache'
 const $prefs = localStorageCache(0, 'app').get('preferences')
@@ -8,30 +9,38 @@ dayjs.extend(timezone)
 export default (dateStr, hideTime, customFormat) => {
   if (!dateStr) return dateStr
 
-  let storedTz = $prefs.data ? $prefs.data.timezone_offset : null
-  let timezoneOffset = storedTz ? Number(storedTz.sign + (Number(storedTz.hours * 60) + Number(storedTz.minutes))) : null
+  // get stored timezone offset from prefs
+  const storedTz = get($prefs, 'data.timezone_offset')
+  // if sign exists, stored timezone offset is valid
+  // load timezone offset as hours from sign, hours, minutes
+  const timezoneOffset = get(storedTz, 'sign') ? Number(storedTz.sign + (Number(storedTz.hours) + Number(storedTz.minutes) / 60)) : null
+  // check if timezone offset is valid, handle 0 (which is a valid offset)
+  const timezoneOffsetValid = timezoneOffset || timezoneOffset == 0
 
+  // convert using only dayjs
   let result
-  let now = new Date()
-  let maxDate = new Date(8640000000000000)
-  let date = new Date(dateStr + 'Z')
-  let isToday = now.toDateString() === date.toDateString()
-  let isThisYear = now.getYear() === date.getYear()
-  let isMaxDate = maxDate.getTime() === date.getTime()
-
-  // if timezoneOffset is set in prefs, apply it, otherwise guess timzone
-  let convertedDate = timezoneOffset ? dayjs.utc(date).utcOffset(timezoneOffset) : dayjs.utc(dateStr).tz(dayjs.tz.guess())
+  const now = dayjs()
+  // offset "now" to calculate isToday/isThisYear in local timezone
+  const nowLocal = timezoneOffsetValid ? now.utcOffset(timezoneOffset) : now.tz(dayjs.tz.guess())
+  // ECMAScript max date
+  const dateMax = dayjs(8640000000000000)
+  const date = dayjs.utc(dateStr)
+  // offset "date" to calculate isToday/isThisYear in local timezone
+  const dateLocal = timezoneOffsetValid ? date.utcOffset(timezoneOffset) : date.tz(dayjs.tz.guess())
+  const isToday = dateLocal.isSame(nowLocal, 'day')
+  const isThisYear = dateLocal.isSame(nowLocal, 'year')
+  const isMaxDate = dateLocal.isSame(dateMax)
 
   if (hideTime) {
     if (isToday) { result = 'Today' }
     else if (isMaxDate) { result = 'Permanent' } // bans
-    else { result = convertedDate.format('MMM D, YYYY') }
+    else { result = dateLocal.format('MMM D, YYYY') }
   }
-  else if (customFormat) result = convertedDate.format(customFormat)
+  else if (customFormat) result = dateLocal.format(customFormat)
   else {
-    if (isToday) { result = 'Today at ' +  convertedDate.format('h:mm A') }
-    else if (isThisYear) { result = convertedDate.format('MMMM D [at] h:mm A') }
-    else { result = convertedDate.format('MMM D, YYYY [at] h:mm A') }
+    if (isToday) { result = 'Today at ' +  dateLocal.format('h:mm A') }
+    else if (isThisYear) { result = dateLocal.format('MMMM D [at] h:mm A') }
+    else { result = dateLocal.format('MMM D, YYYY [at] h:mm A') }
   }
   return result
 }
