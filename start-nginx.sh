5491f8615fd8c1119bdf28379834f9405a1dc849
unenglishable <unenglishable@gmail.com>
2024-06-26 16:54:20 -1000

    refactor(public/index): load backend url from $VUE_APP_BACKEND_URL in production
    when app is compiled without the env variable set, should be set to the
    string "$VUE_APP_BACKEND_URL/config.js"
    
    modified the start-nginx.sh script to also run on index.html, which
    should replace that variable during runtime

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 cp $JSFOLDER/*.js /tmp
+cp index.html /tmp
 
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 for file in /tmp/*.js;
 do
   cat $file | envsubst $EXISTING_VARS | tee $JSFOLDER/$(basename $file)
 done
+cat /tmp/index.html | envsubst $EXISTING_VARS | tee index.html
 rm /tmp/*.js
+rm /tmp/index.html
 nginx -g 'daemon off;'
