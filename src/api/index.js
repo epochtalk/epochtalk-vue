0624a21b2c4ceea0d3c7a7f9fe1da8d2b05ace39
Anthony Kinsey <anthony@slickage.com>
2025-01-17 12:14:27 -1000

    fix(api-key): attempt to fix api key

--- src/api/index.js
+++ src/api/index.js
@@ -19,8 +19,6 @@ const $auth = localStorageCache(0, 'app').get('auth')
 const initUser = $auth ? $auth.data : undefined
 if (initUser) { $axios2.defaults.headers.common['Authorization'] = `BEARER ${initUser.token}` }
 
-$axios2.defaults.headers.common['api-key'] = process.env.VUE_APP_API_KEY || config.VUE_APP_API_KEY
-
 /* provided methods */
 const $http = (path, opts, handleErrors) => {
   opts = opts || {}
@@ -56,6 +54,11 @@ const $http2 = (path, opts, handleErrors) => {
   delete opts.method
   const data = opts.data
   delete opts.data
+
+  $axios2.defaults.headers.common['api-key'] = process.env.VUE_APP_API_KEY || config.VUE_APP_API_KEY
+  $axios2.defaults.headers.common['x-api-key'] = process.env.VUE_APP_API_KEY || config.VUE_APP_API_KEY
+  console.log(process.env.VUE_APP_API_KEY || config.VUE_APP_API_KEY)
+  $axios2.defaults.withCredentials = true;
 
   let req = (m => {
     switch(m) {
