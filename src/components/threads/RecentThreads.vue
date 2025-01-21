891c938b1a22c53db696ef16dc80c6ce3c25c412
Anthony Kinsey <anthony@slickage.com>
2025-01-21 12:28:46 -1000

    refactor(collapsible-recent-threads): wip implement collapsible section for recent threads

--- src/components/threads/RecentThreads.vue
+++ src/components/threads/RecentThreads.vue
@@ -1,64 +1,78 @@
 <template>
   <!-- Recent Threads -->
   <div class="dashboard-hero">
-    <div class="recent-threads">
-      <h1 class="title">Recent Threads</h1>
-
-      <div class="centered-text" v-if="!threads">
-        <h5>No recently updated threads available</h5>
+    <div class="recent-threads collapse-section">
+      <div @click="toggle" class="collapse-section">
+        <a :class="{ 'is-open': collapsedCats.indexOf('recent-threads-00') < 0, 'is-closed': collapsedCats.indexOf('recent-threads-00') > -1 }">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.84 22.63" class="icon__caretDown">
+            <title></title>
+            <g id="Layer_2" data-name="Layer 2">
+              <polyline class="icon" points="37.92 1.92 19.92 19.92 1.92 1.92" />
+            </g>
+          </svg>
+        </a>
+        <h1 class="title">Recent Threads</h1>
       </div>
+      <transition>
+        <div>
+          <div v-if="!threads && collapsedCats.indexOf('recent-threads-00') < 0" class="centered-text">
+            <h5>No recently updated threads available</h5>
+          </div>
 
-      <div class="threads-container" v-if="threads">
-        <!-- Thread listing -->
-        <div class="threads-data threads-recent" v-for="thread in threads" :key="thread.id">
-          <div class="states">
-            <div class="thread-state" v-if="thread.sticky || thread.latest">
-              <span v-if="thread.latest" data-balloon="Unread">
-                <svg class="is-unread" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                  <title></title>
-                  <g id="icons">
-                    <circle cx="16" cy="16" r="16" />
-                  </g>
-                </svg>
-              </span>
-              <span v-if="thread.sticky" data-balloon="Sticky">
-                <svg class="is-sticky" viewBox="0 0 40.68 40.68" xmlns="http://www.w3.org/2000/svg">
-                  <path d="m40 9.92-9.24-9.19a2.5 2.5 0 0 0 -3.54 3.54l9.2 9.19a2.5 2.5 0 0 0 3.58-3.54z" />
-                  <path
-                    d="m12 14.52a3 3 0 0 0 -4.24 4.24l5.66 5.66-11.3 11.31-2.12 4.95 5-2.12 11.26-11.31 5.66 5.66a3 3 0 0 0 4.24-4.25z" />
-                  <path d="m17.46 9.23h16v12h-16z" transform="matrix(.70710678 -.70710678 .70710678 .70710678 -3.31 22.46)" />
-                </svg>
-              </span>
+          <div v-if="threads && collapsedCats.indexOf('recent-threads-00') < 0" class="threads-container">
+            <!-- Thread listing -->
+            <div class="threads-data threads-recent" v-for="thread in threads" :key="thread.id">
+              <div class="states">
+                <div class="thread-state" v-if="thread.sticky || thread.latest">
+                  <span v-if="thread.latest" data-balloon="Unread">
+                    <svg class="is-unread" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+                      <title></title>
+                      <g id="icons">
+                        <circle cx="16" cy="16" r="16" />
+                      </g>
+                    </svg>
+                  </span>
+                  <span v-if="thread.sticky" data-balloon="Sticky">
+                    <svg class="is-sticky" viewBox="0 0 40.68 40.68" xmlns="http://www.w3.org/2000/svg">
+                      <path d="m40 9.92-9.24-9.19a2.5 2.5 0 0 0 -3.54 3.54l9.2 9.19a2.5 2.5 0 0 0 3.58-3.54z" />
+                      <path
+                        d="m12 14.52a3 3 0 0 0 -4.24 4.24l5.66 5.66-11.3 11.31-2.12 4.95 5-2.12 11.26-11.31 5.66 5.66a3 3 0 0 0 4.24-4.25z" />
+                      <path d="m17.46 9.23h16v12h-16z" transform="matrix(.70710678 -.70710678 .70710678 .70710678 -3.31 22.46)" />
+                    </svg>
+                  </span>
+                </div>
+                <div class="thread-status" v-if="thread.locked || thread.poll">
+                  <span v-if="thread.locked" data-balloon="Locked">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                      <title></title>
+                      <path d="M40,21H37.5V16.48a13.5,13.5,0,0,0-27,0V21H8a2,2,0,0,0-2,2V43a2,2,0,0,0,2,2H40a2,2,0,0,0,2-2V23A2,2,0,0,0,40,21ZM15.5,16.48a8.5,8.5,0,0,1,17,0V21h-17Z"/>
+                    </svg>
+                  </span>
+                  <span v-if="thread.poll" data-balloon="Includes a Poll">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                      <path class="cls-1" d="M42,2H6A4,4,0,0,0,2,6V42a4,4,0,0,0,4,4H42a4,4,0,0,0,4-4V6A4,4,0,0,0,42,2ZM13.75,40h-6V32h6Zm9,0h-6V22h6Zm9,0h-6V27h6Zm9,0h-6V12h6Z"/>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+              <div class="title">
+                <router-link :class="{bold: thread.latest}" class="thread-title" :title="decode(thread.title)" :to="{ name: 'Posts', params: { threadSlug: thread.slug, threadId: thread.id } }" onclick="event.stopPropagation()">{{decode(thread.title)}}</router-link>
+              </div>
+              <div class="user">
+                in
+                <router-link class="thread-board" :title="decode(thread.board.name)" :to="{ name: 'Threads', params: { boardSlug: thread.board.slug } }" onclick="event.stopPropagation()">{{decode(thread.board.name)}}</router-link>
+                by
+                <span v-if="thread.deleted">deleted</span>
+                <router-link onclick="event.stopPropagation()" v-if="!thread.deleted" :to="{ path: '/profile/' + thread.user.username.toLowerCase() }">{{thread.user.username}}</router-link>
+              </div>
+              <div class="last-reply">
+                <div>{{humanDate(thread.post.created_at)}}</div>
+              </div>
             </div>
-            <div class="thread-status" v-if="thread.locked || thread.poll">
-              <span v-if="thread.locked" data-balloon="Locked">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-                  <title></title>
-                  <path d="M40,21H37.5V16.48a13.5,13.5,0,0,0-27,0V21H8a2,2,0,0,0-2,2V43a2,2,0,0,0,2,2H40a2,2,0,0,0,2-2V23A2,2,0,0,0,40,21ZM15.5,16.48a8.5,8.5,0,0,1,17,0V21h-17Z"/>
-                </svg>
-              </span>
-              <span v-if="thread.poll" data-balloon="Includes a Poll">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-                  <path class="cls-1" d="M42,2H6A4,4,0,0,0,2,6V42a4,4,0,0,0,4,4H42a4,4,0,0,0,4-4V6A4,4,0,0,0,42,2ZM13.75,40h-6V32h6Zm9,0h-6V22h6Zm9,0h-6V27h6Zm9,0h-6V12h6Z"/>
-                </svg>
-              </span>
-            </div>
-          </div>
-          <div class="title">
-            <router-link :class="{bold: thread.latest}" class="thread-title" :title="decode(thread.title)" :to="{ name: 'Posts', params: { threadSlug: thread.slug, threadId: thread.id } }" onclick="event.stopPropagation()">{{decode(thread.title)}}</router-link>
-          </div>
-          <div class="user">
-            in
-            <router-link class="thread-board" :title="decode(thread.board.name)" :to="{ name: 'Threads', params: { boardSlug: thread.board.slug } }" onclick="event.stopPropagation()">{{decode(thread.board.name)}}</router-link>
-            by
-            <span v-if="thread.deleted">deleted</span>
-            <router-link onclick="event.stopPropagation()" v-if="!thread.deleted" :to="{ path: '/profile/' + thread.user.username.toLowerCase() }">{{thread.user.username}}</router-link>
-          </div>
-          <div class="last-reply">
-            <div>{{humanDate(thread.post.created_at)}}</div>
           </div>
         </div>
-      </div>
+      </transition>
+
     </div>
   </div>
 </template>
@@ -68,8 +82,12 @@ import humanDate from '@/composables/filters/humanDate'
 import decode from '@/composables/filters/decode'
 
 export default {
-  props: ['threads'],
-  setup() { return { humanDate, decode }}
+  props: ['threads', 'collapsedCats'],
+  emits: ['toggle'],
+  setup(props, { emit }) {
+    const toggle = () => emit('toggle', {id: 'recent-threads-00'})
+    return { toggle, humanDate, decode }
+  }
 }
 </script>
 
@@ -169,6 +187,40 @@ export default {
       }
     }
   }
+
+  .collapse-section {
+     @include no-select;
+     cursor: pointer;
+
+    .title { display: inline-block; margin-left: .5rem; }
+
+    .is-open {
+      .icon__caretDown {
+        transform: rotateZ(0deg);
+        transition: all ease-in-out 150ms;
+      }
+    }
+
+    .is-closed {
+      .icon__caretDown {
+        transform: rotateZ(-90deg);
+        transition: all ease-in-out 150ms;
+      }
+    }
+
+     .icon__caretDown {
+       margin-bottom: 4px;
+       width: 8px;
+
+       polyline {
+         fill: none;
+         stroke: $secondary-font-color;
+         stroke-linecap: round;
+         stroke-miterlimit: 10;
+         stroke-width: 7px;
+       }
+      }
+    }
 
   @include break-mobile-sm { padding: 1.5rem 1rem 0; }
 }
