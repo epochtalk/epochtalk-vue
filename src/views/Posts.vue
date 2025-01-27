6f621f2b6c69d8b4a34ebf851593c455a57ba9a5
Anthony Kinsey <anthony@slickage.com>
2025-01-27 12:41:06 -1000

    fix(code-tag): remove decode which was converting escaped gt and lt back into > and <

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -306,7 +306,7 @@
           </div>
           <!-- Post Body -->
           <!-- TODO(akinsey): post-processing="post.body_html" style-fix="true" -->
-          <div class="post-body" :class="{ 'rtl': post.right_to_left }" v-html="decode(post.body_html, true)"></div>
+          <div class="post-body" :class="{ 'rtl': post.right_to_left }" v-html="post.body_html"></div>
           <div v-if="post.user.signature && !disableSignature">
             <!-- TODO(akinsey): post-processing="post.user.signature" style-fix="true" -->
             <div class="post-signature" v-html="post.user.signature"></div>
